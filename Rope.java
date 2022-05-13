1a7f1934d1e83e2a469772f3c4043227a7a0ad74
Morris <jordanm27378@stu.powayusd.com>
2022-05-13 12:41:24 -0700

    finished

--- Rope.java
+++ Rope.java
@@ -1,23 +1,31 @@
 import java.util.ArrayList;
 
 public class Rope {
-    
+
+    //Length of the rope
     private int length;
+    //all ropes are initialized as straight and ready to be tied
     private boolean straight = true;
+    //the material of the rope
     private String type = "normal";
+    //the type of knot of the rope
     private String knot = "rope";
+    //list of "contestants" in a rope competition
     public static ArrayList<Rope> contestants = new ArrayList<Rope>();
 
+    //Creates a rope with a length
     public Rope(int ropeLength) {
         length = ropeLength;
     }
 
+    //Creates a rope object with a length and a knot
     public Rope(int ropeLength, String ropeKnot) {
         length = ropeLength;
         knot = ropeKnot;
         straight = false;
     }
 
+    //Creates a rope object with a length, material, and knot
     public Rope(int ropeLength, String ropeType, String ropeKnot) {
         length = ropeLength;
         type = ropeType;
@@ -26,6 +34,7 @@ public class Rope {
 
     }
 
+    //Creates a rope object with a length, material, and if the rope is straight/able to be knotted
     public Rope(int ropeLength, String ropeType, boolean ropeStraight) {
         straight = ropeStraight;
         length = ropeLength;
@@ -48,30 +57,35 @@ public class Rope {
         return knot;
     }
 
+    //Sets the rope object's material
     public void setType(String ropeType) {
         System.out.print(this + " is now ");
         type = ropeType;
         System.out.println(this);
     }
 
+    //Sets the rope object's knot
     public void setKnot(String ropeKnot) {
         System.out.print(this + " is now ");
         knot = ropeKnot;
         System.out.println(this);
     }
 
+    //Sets the rope object to be straight (ready to be knotted)
     public void straighten() {
         straight = true;
         System.out.println("The " + this + " is straightened");
         knot = "rope";
     }
 
+    //Coils the rope. Essentially a placeholder for a knot
     public void coil() {
         straight = false;
         System.out.println("The " + this + " is coiled");
         knot = "coil";
     }
 
+    //Shortens the rope by the designated length and also undos the knot it had
     public void cut(int n) {
         if (n < length) {
             System.out.print("The " + this + " is now ");
@@ -82,6 +96,7 @@ public class Rope {
         }
     }
 
+    //If any rope in contestants does not have a knot (or coiled) it is knotted
     public static void knotAll() {
         for (Rope r : contestants) {
             if (r.getKnot().equals("rope")) {
@@ -90,6 +105,7 @@ public class Rope {
         }
     }
 
+    //Removes any knots that would be "illegal" for the competition
     public static void removeIllKnot(String illegalKnot) {
         for (int i = 0; i < contestants.size(); i++) {
             if (contestants.get(i).getKnot().equals(illegalKnot)) {
@@ -99,31 +115,40 @@ public class Rope {
         }
     }
 
+    //Generates a random knot name
     public static String randomKnot() {
         String [] knotList = {"Sailor knot", "Slipknot", "Half-hitch knot", "Clove-hitch knot"};
         int ran = (int) (Math.random() * knotList.length);
         return knotList[ran];
     }
 
+    //Generates a random material name
     public static String randomType() {
         String[] ropeList = {"Nylon", "Bungee", "Synthetic", "Cotton", "Hemp"};
         int ran = (int) (Math.random() * ropeList.length);
         return ropeList[ran];
     }
 
+    //Adds a rope to contestants
     public static void addRope(Rope r) {
         contestants.add(r);
         System.out.println("A " + r + " has entered the competition.");
     }
 
+    //Eliminates a rope from the competition
     public static void removeRope(int index) {
         System.out.println(contestants.remove(index) + " has left the running.");
     }
 
+    //Prints out the longest rope(s) of the contest
     public static void longestRope() {
+        //Initializes max as the length of the first item
         int max = contestants.get(0).getLength();
+        //List of indices that will hold indices of ropes that have a length equal to the longest length
         ArrayList<Integer> index = new ArrayList<Integer>();
+        //Initializes the index list to have the first item (like max)
         index.add(0);
+        //for every rope in contestants, check and see if their length is greater than or equal to the max
         for (int i = 1; i < contestants.size(); i++) {
             if (contestants.get(i).getLength() == max) {
                 index.add(i);
@@ -136,15 +161,18 @@ public class Rope {
             }
         }
 
+        //print the winners
         if (index.size() == 1) {
             System.out.println("The winner is " + contestants.get(index.get(0)));
         }
+
+        //if multiple winners, print all of them
         else {
             System.out.print("The winners are");
             for (int n : index) {
                 System.out.print(" " + contestants.get(n));
             }
-            System.out.println();
+            System.out.println(".");
         }
         
     }
--- RopeRunner.java
+++ RopeRunner.java
@@ -6,19 +6,26 @@ public class RopeRunner {
         Rope.addRope(new Rope(3, "Nylon", Rope.randomKnot()));
         Rope.addRope(new Rope(7, Rope.randomType(), true));
         Rope.addRope(new Rope(6));
+        //"unties" any illegal knots
         Rope.removeIllKnot(Rope.randomKnot());
+        //tests the setKnot and randomKnot methods
         Rope.contestants.get(0).setKnot(Rope.randomKnot());
+        //for every rope in contestants; either coil it or if it has a knot (and is longer than 5 inches),
+        // cut it and then knot it again (not taking into account any illegal knottery)
         for (Rope r : Rope.contestants) {
             if (r.getKnot().equals("rope")) {
                 r.coil();
             }
 
             if (r.getLength() > 5) {
+                //makes sure the rope is only cut at a minimum of 1 and a maximum of 1 less than its length
                 r.cut((int) (Math.random() * (r.getLength()-1) + 1));
                 r.setKnot(Rope.randomKnot());
             }
         }
+        //reknot all of the cut rope
         Rope.knotAll();
+        //find the winner
         Rope.longestRope();
     }
 }
